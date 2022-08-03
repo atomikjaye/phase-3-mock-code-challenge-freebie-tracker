c475ee7c4de7663e6749626cbe27c0ba1b9cffd5
Jocelyn Naarden <tzionone@gmail.com>
2022-08-03 01:26:45 -0400

    Finished Assignment ✔

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,4 +1,13 @@
 class Dev < ActiveRecord::Base
   has_many :freebies
   has_many :companies, through: :freebies
+
+  def recieved_one?(item_name)
+    self.freebies.exists?(item_name: item_name)
+  end
+
+  def give_away(new_dev, freebie)
+    freebie.dev_id = new_dev.id
+    freebie.save
+  end
 end
